bd0a52b00f3be90e4d243b74a7896cc92d0cb686
taddeimania <jtaddei@gmail.com>
2017-08-07 09:27:47 -0400

    initial

---
+++ src/main/java/io/joel/SecretkeeperApplication.java
@@ -0,0 +1,12 @@
+package io.joel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretkeeperApplication.class, args);
+	}
+}
