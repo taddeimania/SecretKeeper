6c889ab67b5390dc9f0a2a35d8431b605271f9d1
taddeimania <jtaddei@gmail.com>
2017-08-07 10:19:15 -0400

    Started user and role models. Calm before the storm.

---
+++ src/main/java/io/joel/models/User.java
@@ -0,0 +1,50 @@
+package io.joel.models;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
