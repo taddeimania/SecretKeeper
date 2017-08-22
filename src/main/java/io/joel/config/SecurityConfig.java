52e4eec7f6655e5c0b48e5619e7d281181f83aa3
taddeimania <jtaddei@gmail.com>
2017-08-22 13:36:16 -0400

    Step 2: Configured spring security to use password hasher

--- src/main/java/io/joel/config/SecurityConfig.java
+++ src/main/java/io/joel/config/SecurityConfig.java
@@ -2,19 +2,42 @@ package io.joel.config;
 
 import io.joel.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Qualifier("dataSource")
+    @Autowired
+    DataSource dataSource;
+
+    private String userQuery = "select username, password from user_data u where u.username = ?";
+    private String roleQuery = "select u.username, r.name from user_data u inner join role r on u.role_id = r.id where u.username = ?";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery(userQuery)
+                .authoritiesByUsernameQuery(roleQuery)
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
