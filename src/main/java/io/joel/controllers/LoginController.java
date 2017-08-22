ee1f19539fda0a5c7eed5348736a5e40e6137560
taddeimania <jtaddei@gmail.com>
2017-08-22 13:58:55 -0400

    Step 5: Set most properties on new user instance. TODO: Make a role repo

--- src/main/java/io/joel/controllers/LoginController.java
+++ src/main/java/io/joel/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package io.joel.controllers;
 
 import io.joel.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginForm(Model model, HttpServletRequest request) {
@@ -33,6 +38,12 @@ public class LoginController {
     public String signupForm(@RequestParam("username") String username,
                              @RequestParam("password") String password,
                              Model model) {
+        User user = new User();
+        user.setUsername(username);
+        String encryptedPassword = bCryptPasswordEncoder.encode(password);
+        user.setPassword(encryptedPassword);
+        // Step 5: Set the user role on the user instance
+        // Step 6: Save user to the database!
         return "redirect:/login";
     }
 }
